4a9c0c6a52f86adceb21522915069322c7c1dc47
Jay <132546990+theonejlove@users.noreply.github.com>
2023-05-19 10:17:18 +0900

    deleted and updated files

--- config.py
+++
@@ -1,3 +0,0 @@
-# FOR TEQUILA
-ENDPOINT = "https://tequila-api.kiwi.com"
-API_KEY = "Qsn2AVK13lWtLDB8speMzCp5ngwzGUB2"
--- flight_search.py
+++ flight_search.py
@@ -1,9 +1,10 @@
 import requests
 from flight_data import FlightData
-import config
+import os
 
-TEQUILA_ENDPOINT = config.ENDPOINT
-TEQUILA_API_KEY = config.API_KEY
+TEQUILA_ENDPOINT = os.environ("TEQUILA_ENDPOINT")
+TEQUILA_API_KEY=os.environ("TEQUILA_API_KEY")
+
 class FlightSearch:
 
     def get_destination_code(self, city_name):
