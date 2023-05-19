4372d8821b5cfd0014fe71b1cf58508553ddffd2
Jay <132546990+theonejlove@users.noreply.github.com>
2023-05-19 10:00:36 +0900

    Initial commit

---
+++ config.py
@@ -0,0 +1,3 @@
+# FOR TEQUILA
+ENDPOINT = "https://tequila-api.kiwi.com"
+API_KEY = "Qsn2AVK13lWtLDB8speMzCp5ngwzGUB2"
---
+++ data_manager.py
@@ -0,0 +1,41 @@
+
+import requests
+
+SHEETY_PRICES_ENDPOINT = "https://api.sheety.co/97cff91d3400906d57ac23bf7067ab82/flightDeals/prices"
+headers = {
+    "Authorization": "Bearer flightClub"
+}
+SHEET_USERS_ENDPOINT = "https://api.sheety.co/97cff91d3400906d57ac23bf7067ab82/flightDeals/users"
+
+class DataManager:
+
+    def __init__(self):
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        response = requests.get(url=SHEETY_PRICES_ENDPOINT, headers=headers)
+        data = response.json()
+        print(data)
+        self.destination_data = data["prices"]
+        return self.destination_data
+
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(
+                url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
+                json=new_data,
+                headers=headers
+            )
+            print(response.text)
+
+    def get_customer_emails(self):
+        customers_endpoint = SHEET_USERS_ENDPOINT
+        response = requests.get(customers_endpoint)
+        data = response.json()
+        self.customer_data = data["users"]
+        return self.customer_data
